096a9dace5db1e5c66e8e39a5d2a1283a219276e
Evan Ordona <eordona@umd.edu>
2024-04-11 02:06:51 -0400

    setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen w-screen flex justify-center items-center text-5xl'>
+        Hello World
+      </div>
+   
+    </>
+  )
+}
+
+export default App
